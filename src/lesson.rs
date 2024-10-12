46c6db79518ad6f3bcbd2340d97a6331b6cf08fb
learturely <learturely@gmail.com>
2024-10-13 00:35:56 +0800

    init.

---
+++ src/lesson.rs
@@ -0,0 +1,119 @@
+// Copyright (C) 2024 learturely <learturely@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use cxsign::user::Session;
+use indicatif::MultiProgress;
+use serde::{Deserialize, Serialize};
+
+use super::tools::{
+    arc_into_inner_error_handler, json_parsing_error_handler, mutex_into_inner_error_handler,
+    prog_init_error_handler, VideoPath,
+};
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+struct Time_ {
+    time: i64,
+}
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct Lesson {
+    #[serde(rename = "startTime")]
+    start_time: Time_,
+    id: i64,
+}
+
+impl Lesson {
+    pub fn get_start_time(&self) -> i64 {
+        self.start_time.time
+    }
+    pub fn get_live_id(&self) -> i64 {
+        self.id
+    }
+    pub fn get_recording_url(
+        session: &Session,
+        live_id: i64,
+    ) -> Result<VideoPath, Box<ureq::Error>> {
+        crate::tools::get_recording_live_video_path(session, live_id)
+    }
+    pub fn get_all_lessons(session: &Session, live_id: i64) -> Result<Vec<i64>, Box<ureq::Error>> {
+        let mut lessons: Vec<Lesson> = crate::protocol::list_single_course(session, live_id)?
+            .into_json()
+            .unwrap_or_else(json_parsing_error_handler);
+        lessons.sort_by_key(|l| l.get_start_time());
+        Ok(lessons.into_iter().map(|l| l.get_live_id()).collect())
+    }
+    pub fn get_recording_lives(
+        session: &Session,
+        live_id: i64,
+        multi: &MultiProgress,
+    ) -> Result<HashMap<i64, VideoPath>, Box<ureq::Error>> {
+        let lessons: Vec<Lesson> = crate::protocol::list_single_course(session, live_id)?
+            .into_json()
+            .unwrap_or_else(json_parsing_error_handler);
+        let total = lessons.len();
+        let thread_count = total / 64;
+        let rest_count = total % 64;
+        let sty = indicatif::ProgressStyle::with_template(
+            "获取回放地址：[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+        )
+        .unwrap_or_else(prog_init_error_handler);
+        let pb = multi.add(indicatif::ProgressBar::new(total as u64));
+        pb.set_style(sty);
+        let pb = Arc::new(Mutex::new(pb));
+        let paths = Arc::new(Mutex::new(HashMap::new()));
+        let mut handles = Vec::new();
+        for block in 0..64 {
+            let ref_ = if block < rest_count {
+                ((thread_count + 1) * block)..((thread_count + 1) * (block + 1))
+            } else {
+                (thread_count * block + rest_count)..(thread_count * (block + 1) + rest_count)
+            };
+            let session = (*session).clone();
+            let paths = Arc::clone(&paths);
+            let pb = Arc::clone(&pb);
+            let mut lessons_ = vec![];
+            for lesson in &lessons[ref_] {
+                lessons_.push(lesson.clone())
+            }
+            let handle = std::thread::spawn(move || {
+                for lesson in lessons_ {
+                    if let Ok(path) = Lesson::get_recording_url(&session, lesson.get_live_id()) {
+                        paths.lock().unwrap().insert(lesson.get_start_time(), path);
+                    }
+                    pb.lock().unwrap().inc(1);
+                }
+            });
+            handles.push(handle);
+        }
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        let paths = Arc::into_inner(paths)
+            .unwrap_or_else(arc_into_inner_error_handler)
+            .into_inner()
+            .unwrap_or_else(mutex_into_inner_error_handler);
+        let pb = Arc::into_inner(pb)
+            .unwrap_or_else(arc_into_inner_error_handler)
+            .into_inner()
+            .unwrap_or_else(mutex_into_inner_error_handler);
+        pb.finish_with_message("获取回放地址完成。");
+        multi.remove(&pb);
+        Ok(paths)
+    }
+}
---
+++ src/protocol.rs
@@ -0,0 +1,69 @@
+// Copyright (C) 2024 learturely <learturely@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use cxsign::user::Session;
+use ureq::{Agent, Response};
+
+static GET_VIEW_URL_HLS: &str = "http://newesxidian.chaoxing.com/live/getViewUrlHls";
+pub fn get_view_url_hls(agent: &Agent, live_id: i64) -> Result<Response, Box<ureq::Error>> {
+    let url = format!("{GET_VIEW_URL_HLS}?liveId={live_id}&status=2&jie=&isStudent=");
+    Ok(agent.get(&url).call()?)
+}
+static LIST_STUDENT_COURSE_LIVE_PAGE: &str =
+    "http://newesxidian.chaoxing.com/frontLive/listStudentCourseLivePage";
+pub fn list_student_course_live_page(
+    session: &Session,
+    week: i64,
+    term_year: i32,
+    term: i32,
+) -> Result<Response, Box<ureq::Error>> {
+    let url = format!(
+        "{LIST_STUDENT_COURSE_LIVE_PAGE}?fid=16820&userId={}&week={week}&termYear={term_year}&termId={term}&type=1",
+        session.get_uid(),
+    );
+    Ok(session.get(&url).call()?)
+}
+static LIST_SINGLE_COURSE: &str = "http://newesxidian.chaoxing.com/live/listSignleCourse";
+pub fn list_single_course(session: &Session, live_id: i64) -> Result<Response, Box<ureq::Error>> {
+    let url = format!(
+        "{LIST_SINGLE_COURSE}?fid=16820&liveId={live_id}&uId={}",
+        session.get_uid()
+    );
+    Ok(session.get(&url).call()?)
+}
+
+static GET_VIEW_URL: &str = "http://newesxidian.chaoxing.com/live/getViewUrlNoCourseLive";
+pub fn get_live_url(agent: &Agent, device_conde: &str) -> Result<Response, Box<ureq::Error>> {
+    let url = format!("{GET_VIEW_URL}?deviceCode={device_conde}&status=1&fid=16820");
+    Ok(agent.get(&url).call()?)
+}
+// pub fn get_recording_url(
+//     agent: &Agent,
+//     device_conde: &str,
+//     start_time: &str,
+//     end_time: &str,
+// ) -> Result<Response, ureq::Error> {
+//     let url = format!("{GET_VIEW_URL}?deviceCode={device_conde}&status=2&fid=16820&startTime={start_time}&endTime={end_time}");
+//     agent.get(&url).call()
+// }
+static GET_WEEK_DETAIL: &str = "http://newesxidian.chaoxing.com/frontLive/getWeekDetail";
+pub fn get_week_detail(
+    agent: &Agent,
+    week: i32,
+    semester_id: i32,
+) -> Result<Response, Box<ureq::Error>> {
+    let url = format!("{GET_WEEK_DETAIL}?week={week}&semesterId={semester_id}");
+    Ok(agent.get(&url).call()?)
+}
